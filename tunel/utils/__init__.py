4efc9738a5c8f9984ec1005bd3903fdb32d557a9
vsoch <vsoch@users.noreply.github.com>
2022-08-09 19:11:09 -0600

    black
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- tunel/utils/__init__.py
+++ tunel/utils/__init__.py
@@ -1,7 +1,26 @@
-from .fileio import (copyfile, get_file_hash, get_tmpdir, get_tmpfile, mkdir_p,
-                     mkdirp, print_json, read_config_file, read_file,
-                     read_json, read_lines, read_yaml, recursive_find,
-                     write_file, write_json)
+from .fileio import (
+    copyfile,
+    get_file_hash,
+    get_tmpdir,
+    get_tmpfile,
+    mkdir_p,
+    mkdirp,
+    print_json,
+    read_config_file,
+    read_file,
+    read_json,
+    read_lines,
+    read_yaml,
+    recursive_find,
+    write_file,
+    write_json,
+)
 from .generate import namer
-from .terminal import (check_install, confirm_action, confirm_uninstall,
-                       get_installdir, run_command, which)
+from .terminal import (
+    check_install,
+    confirm_action,
+    confirm_uninstall,
+    get_installdir,
+    run_command,
+    which,
+)
